2bcfef401b9e58c35d1b93a79d809f987b1c92e8
Jefffer <jefre123@hotmail.com>
2024-12-28 23:00:09 +0100

    Updated logic to take in account the quantity of books in the total price.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,32 +2,41 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart, onRemoveFromCart }) => {
+//   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
   return (
     <aside className="cart-container">
       <h3>Carrito 🛒</h3>
       {cart.length > 0 ? (
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id} className="cart-item">
-              <div id="item-details">
-                <strong>{item.title}</strong>
-                {item.price.toFixed(2)}€
-                <div className="item-quantity">
-                  <button className="quantity-btn">-</button>
-                  <span id="quantity">{item.quantity}</span>
-                  <button className="quantity-btn">+</button>
+        <>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id} className="cart-item">
+                <div id="item-details">
+                  <strong>{item.title}</strong>
+                  {item.price.toFixed(2)}€
+                  <div className="item-quantity">
+                    <button className="quantity-btn">-</button>
+                    <span id="quantity">{item.quantity}</span>
+                    <button className="quantity-btn">+</button>
+                  </div>
                 </div>
-              </div>
-              <button
-                id="remove-btn"
-                className="btn btn-danger btn-sm"
-                onClick={() => onRemoveFromCart(item.id)}
-              >
-                🗑️
-              </button>
-            </li>
-          ))}
-        </ul>
+                <button
+                  id="remove-btn"
+                  className="btn btn-danger btn-sm"
+                  onClick={() => onRemoveFromCart(item.id)}
+                >
+                  🗑️
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="cart-total d-flex justify-content-between align-items-center mt-3">
+            <span>Total: </span>
+            <span className="total-price">${totalPrice.toFixed(2)}€</span>
+          </div>
+        </>
       ) : (
         <p>
           <i>El carrito está vacío</i>
