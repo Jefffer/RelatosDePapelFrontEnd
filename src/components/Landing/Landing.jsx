b772a79d147c0aaf5f6c6eaddf4545d752067d3d
Jefffer <jefre123@hotmail.com>
2025-02-28 18:04:34 +0100

    Landing corrections.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,55 +1,93 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Landing.css";
+import { Typography, Box } from "@mui/material";
 
 const Landing = () => {
-  const navigate = useNavigate();
-  const [countdown, setCountdown] = useState(5);
+    const navigate = useNavigate();
+    const [countdown, setCountdown] = useState(5);
 
-  // Contador
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/main");
-    }, 5000);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/main");
+        }, 5000);
 
-    // Para la limpieza del temporizador
-    return () => clearTimeout(timer); 
-  }, [navigate]);
+        return () => clearTimeout(timer);
+    }, [navigate]);
 
-  // Actualizador del contador en tiempo real
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCountdown((prevCountdown) => prevCountdown - 1);
-    }, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCountdown((prevCountdown) => prevCountdown - 1);
+        }, 1000);
 
-    // Para realizar la limpieza del intervalo
-    return () => clearInterval(interval); 
-  }, []);
+        return () => clearInterval(interval);
+    },);
 
-  return (
-    <div className="access-view">
-      <div className="content-container text-center d-flex flex-column justify-content-center align-items-center vh-100">
-        <h1 className="title display-4 mb-3">RELATOS DE PAPEL 📔</h1>
-        <div className="subtitle">
-          {/* <p className="my-name">Por: Jefferson Rodríguez</p> */}
-          <a id="my-name" href="https://github.com/Jefffer" target="_blank">
-          {" "}
-          POR JEFFERSON RODRIGUEZ {" "} 
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-            alt="GitHub"
-          />
-        </a>
-        <br />
-        <br />
-          
-          <strong>
-            Serás redirigido a la página principal en <span id="countdown">{countdown}</span> segundos...
-          </strong>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <Box
+            sx={{
+                backgroundColor: "#8298c6",
+                width: "100vw",
+                height: "100vh",
+                margin: 0,
+                padding: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Box
+                sx={{
+                    textAlign: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    color: "#e9e7db",
+                }}
+            >
+                <Typography variant="h1" sx={{ fontSize: "5.5rem", marginBottom: "1rem" }}>
+                    RELATOS DE PAPEL 📔
+                </Typography>
+                <Box sx={{ fontSize: "1.3rem" }}>
+                    <a
+                        id="my-name"
+                        href="https://github.com/Jefffer"
+                        target="_blank"
+                        style={{
+                            color: 'inherit', // Mantener el color del texto
+                            textDecoration: 'none', // Eliminar subrayado
+                        }}
+                    >
+                        POR JEFFERSON RODRIGUEZ{" "}
+                        {/* <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                            alt="GitHub"
+                        /> */}
+                    </a>
+                    <br />
+                    <br />
+                    <Typography variant="body1">
+                        Serás redirigido a la página principal en{" "}
+                        <span
+                            style={{
+                                fontSize: "2rem",
+                                fontWeight: "bold",
+                                color: "#ff6347",
+                                backgroundColor: "#e9e7db",
+                                padding: "10px 20px",
+                                borderRadius: "10px",
+                                marginTop: "10px",
+                                display: "inline-block",
+                            }}
+                        >
+                            {countdown}
+                        </span>{" "}
+                        segundos...
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
 };
 
 export default Landing;
